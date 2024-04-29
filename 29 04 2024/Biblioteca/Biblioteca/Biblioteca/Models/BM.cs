5e23ff286ae5d38b52ce8224f5a39ae8e1a16cda
brunoxing2004 <mail@brunosilva.co.uk>
2024-04-29 21:54:11 +0100

    Update BM

--- 29 04 2024/Biblioteca/Biblioteca/Biblioteca/Models/BM.cs
+++ 29 04 2024/Biblioteca/Biblioteca/Biblioteca/Models/BM.cs
@@ -6,5 +6,41 @@ using System.Threading.Tasks;
 
 namespace Biblioteca.Models {
     public class BM {
+        private List<Requisicao> _lstRequisicao;
+        private string _nome;
+
+        //RN1: _nome < 3 --» _nome = "A definir"
+
+        public string Nome
+        {
+            get { return _nome; }
+            set
+            {
+                _nome = value.Trim();
+                if (_nome.Length < 3) _nome = "A definir";
+            }
+        }
+
+        public BM()
+        {
+            Nome = "";
+            _lstRequisicao = new List<Requisicao>();
+        }
+
+        public void Adicionar(Requisicao requisicao)
+        {
+            _lstRequisicao.Add(requisicao);
+        }
+
+        public string exportar()
+        {
+            string resultado = $"<nome>{Nome}</nome>\n";
+            resultado += "<requisicao>\n";
+            foreach(Requisicao r in _lstRequisicao){
+                resultado += $"{r.exportar()}\n";
+            }
+            resultado += "</requisicao>\n";
+            return resultado;
+        }
     }
 }
