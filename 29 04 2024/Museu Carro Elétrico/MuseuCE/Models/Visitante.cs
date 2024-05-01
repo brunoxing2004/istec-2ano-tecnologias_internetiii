58830c4aada612951bb423b3763e066406c62862
brunoxing2004 <mail@brunosilva.co.uk>
2024-05-01 20:26:21 +0100

    Classe Visitante

--- 29 04 2024/Museu Carro Elétrico/MuseuCE/Models/Visitante.cs
+++ 29 04 2024/Museu Carro Elétrico/MuseuCE/Models/Visitante.cs
@@ -8,5 +8,41 @@ namespace MuseuCE.Models
 {
     public class Visitante
     {
+        private string _nome;
+        private string _id;
+
+        //RN1: _nome < 2 caracteres --» "Visitante"
+        //RN2: _id < 6 caracteres --» "******"
+
+        public string Nome
+        {
+            get { return _nome; }
+            set
+            {
+                _nome = value.Trim();
+                if (_nome.Length < 2) _nome = "Visitante";
+            }
+        }
+
+        public string Id
+        {
+            get { return Convert.ToString(_id); }
+            set
+            {
+                _id = value.Trim();
+                if (_id.Length < 6) _id = "******";
+            }
+        }
+
+        public Visitante()
+        {
+            Nome = "";
+            Id = "";
+        }
+
+        public string exportar()
+        {
+            return $"<nome>{Nome}</nome><idvisitante>{Id}</idvisitante>";
+        }
     }
 }
