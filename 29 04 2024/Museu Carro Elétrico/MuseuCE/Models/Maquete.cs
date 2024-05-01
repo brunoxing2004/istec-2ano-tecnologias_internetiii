520e946f13ea70feffac1a76cfbba76cc0e4c3d0
brunoxing2004 <mail@brunosilva.co.uk>
2024-05-01 21:10:14 +0100

    Classe Maquete

--- 29 04 2024/Museu Carro Elétrico/MuseuCE/Models/Maquete.cs
+++ 29 04 2024/Museu Carro Elétrico/MuseuCE/Models/Maquete.cs
@@ -8,6 +8,28 @@ namespace MuseuCE.Models
 {
     public class Maquete : Exposicao
     {
+        private string _tipo;
+        //RN1: tipo < 2 --» "Classificação de tipo pendente"
+        public string Tipo
+        {
+            get { return _tipo; }
+            set
+            {
+                _tipo = value.Trim();
+                if ( _tipo.Length < 2 ) _tipo = "Classificação de tipo pendente";
+            }
+        }
 
+        public Maquete() : base()
+        {
+            Tipo = "";
+        }
+
+        public override string exportar()
+        {
+            string export = base.exportar();
+            export += $"<tipomaquete>{Tipo}</tipomaquete>";
+            return export;
+        }
     }
 }
