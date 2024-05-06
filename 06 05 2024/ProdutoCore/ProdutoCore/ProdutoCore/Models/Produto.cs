ce42837c42e3587562398e512afb26820c6106c5
brunoxing2004 <mail@brunosilva.co.uk>
2024-05-06 21:40:14 +0100

    ProdutoCore - Classe Produto e Config Inicial

---
+++ 06 05 2024/ProdutoCore/ProdutoCore/ProdutoCore/Models/Produto.cs
@@ -0,0 +1,21 @@
+﻿namespace ProdutoCore.Models
+{
+    public class Produto
+    {
+        private string _id;
+        private string _nome;
+        public string _stock;
+
+        public string Nome
+        {
+            get { return _nome; }
+            set
+            {
+                _nome = value.Trim();
+                if (_nome.Length < 3) _nome = "Produto desconhecido";
+            }
+        }
+
+        public string 
+    }
+}
