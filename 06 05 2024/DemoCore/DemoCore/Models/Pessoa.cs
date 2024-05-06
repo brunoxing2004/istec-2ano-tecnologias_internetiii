f92f0066d1932ce4e15d32e3931f11f5e09d1562
brunoxing2004 <mail@brunosilva.co.uk>
2024-05-06 20:59:11 +0100

    Classe Pessoa

--- 06 05 2024/DemoCore/DemoCore/Models/Pessoa.cs
+++ 06 05 2024/DemoCore/DemoCore/Models/Pessoa.cs
@@ -2,6 +2,10 @@
 {
     public class Pessoa
     {
-        public int Id { get; set; }
+        public string Id { get; set; }
+
+        public string Nome { get; set; }
+
+        public string Nif { get; set; }
     }
 }
