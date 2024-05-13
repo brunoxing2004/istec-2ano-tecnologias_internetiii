c0640b42a7b79c5042f13d5226e966cd19329bcb
brunoxing2004 <mail@brunosilva.co.uk>
2024-05-13 21:33:53 +0100

    Classe Documento

---
+++ 13 05 2024/DemoObjetos/DemoObjetos/Models/Documento.cs
@@ -0,0 +1,16 @@
+﻿namespace DemoObjetos.Models
+{
+    public class Documento
+    {
+        public Guid Id { get; set; }
+        public string Titulo { get; set; }
+        public string Resumo { get; set; }
+        public Boolean Estado { get; set; }
+
+        public Documento()
+        {
+            
+        }
+
+    }
+}
--- 13 05 2024/DemoObjetos/DemoObjetos/obj/Debug/net8.0/DemoObjetos.AssemblyInfo.cs
+++ 13 05 2024/DemoObjetos/DemoObjetos/obj/Debug/net8.0/DemoObjetos.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DemoObjetos")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+446169ead75e7903e2c92f6eb3b53f29dcadfb7e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ab80a84cd80d56502cc3acb995dd36b5dbe4cf7b")]
 [assembly: System.Reflection.AssemblyProductAttribute("DemoObjetos")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DemoObjetos")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
