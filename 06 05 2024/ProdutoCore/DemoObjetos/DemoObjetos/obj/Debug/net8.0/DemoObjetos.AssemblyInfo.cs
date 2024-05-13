446169ead75e7903e2c92f6eb3b53f29dcadfb7e
brunoxing2004 <mail@brunosilva.co.uk>
2024-05-13 20:51:05 +0100

    Tabela da Tabuada

--- 06 05 2024/ProdutoCore/DemoObjetos/DemoObjetos/obj/Debug/net8.0/DemoObjetos.AssemblyInfo.cs
+++ 06 05 2024/ProdutoCore/DemoObjetos/DemoObjetos/obj/Debug/net8.0/DemoObjetos.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DemoObjetos")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b16a6f3d40d8f635f2d9eba318b2d5f6a596da95")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+81e04a1b589a7989d3ec920999a08ace680099d7")]
 [assembly: System.Reflection.AssemblyProductAttribute("DemoObjetos")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DemoObjetos")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
