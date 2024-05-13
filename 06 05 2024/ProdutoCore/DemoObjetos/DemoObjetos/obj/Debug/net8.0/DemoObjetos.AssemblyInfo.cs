81e04a1b589a7989d3ec920999a08ace680099d7
brunoxing2004 <mail@brunosilva.co.uk>
2024-05-13 19:54:11 +0100

    Razor - incorporar server-side C/C# em HTML

--- 06 05 2024/ProdutoCore/DemoObjetos/DemoObjetos/obj/Debug/net8.0/DemoObjetos.AssemblyInfo.cs
+++ 06 05 2024/ProdutoCore/DemoObjetos/DemoObjetos/obj/Debug/net8.0/DemoObjetos.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DemoObjetos")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+34d3d75cb840b16e27c53c4f04feaaf13188493e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b16a6f3d40d8f635f2d9eba318b2d5f6a596da95")]
 [assembly: System.Reflection.AssemblyProductAttribute("DemoObjetos")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DemoObjetos")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
