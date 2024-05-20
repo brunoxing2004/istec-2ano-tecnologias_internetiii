23717521cc3fc89b58883e14c9a3440e8b2408ae
brunoxing2004 <mail@brunosilva.co.uk>
2024-05-20 19:39:30 +0100

    Classe ItemDropdown

--- 20 05 2024/DemoObjetos/Models/ItemDropdown.cs
+++ 20 05 2024/DemoObjetos/Models/ItemDropdown.cs
@@ -2,5 +2,12 @@
 {
     public class ItemDropdown
     {
+        public Guid Id { get; set; }
+        public string Designacao { get; set; }
+        public ItemDropdown()
+        {
+            Id = Guid.NewGuid();
+            Designacao = "";
+        }
     }
 }
