9c569ae85a42e8edafb5cb755673c161981482ee
brunoxing2004 <mail@brunosilva.co.uk>
2024-06-03 18:42:22 +0100

    Inicial CoreSQL

---
+++ 03 06 2024/CoreSQL/CoreSQL/Controllers/DocumentoController.cs
@@ -0,0 +1,69 @@
+﻿using CoreSQL.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoreSQL.Controllers {
+    public class DocumentoController : Controller {
+        public IActionResult Listar(string op) {
+            string ligacao = Program.conexaoGlobal;
+            DocumentoHelper dh = new DocumentoHelper(ligacao);
+            int estadoAVer = 0;
+            switch(op) {
+                case "":
+                case "ativos":
+                    estadoAVer = 1;
+                    break;
+                case "inativos":
+                    estadoAVer = 0;
+                    break;
+                case "todos":
+                    estadoAVer = 2;
+                    break;
+                default:
+                    estadoAVer = 1;
+                    break;
+            }
+            List<Documento> lista = dh.list(estadoAVer);
+            return View(lista);
+        }
+
+        [HttpGet]
+        public IActionResult Criar() {
+            //string ligacao = Program.conexaoGlobal;
+            //DocumentoHelper dh = new DocumentoHelper(ligacao);
+            //Documento doc = new Documento { 
+            //    DtPublicacao = DateTime.Now, 
+            //    Estado = 1, 
+            //    Resumo = "Criado automaticamente mas com guids distintos", 
+            //    Titulo = "Criado Automatico" };
+            //dh.save(doc);
+            //return RedirectToAction("Listar", "Documento");
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Criar(Documento documento) {
+            string ligacao = Program.conexaoGlobal;
+            DocumentoHelper dh = new DocumentoHelper(ligacao);
+            dh.save (documento);
+            return RedirectToAction("Listar", "Documento");
+        }
+
+        [HttpGet]
+        public IActionResult Editar(string op) {
+            string ligacao = Program.conexaoGlobal;
+            DocumentoHelper dh = new DocumentoHelper(ligacao);
+            Documento? doc = dh.get(op);
+            if (doc == null) return RedirectToAction("Listar", "Documento");
+            return View(doc);
+        }
+
+        [HttpPost]
+        public IActionResult Editar (Documento documento) {
+            string ligacao = Program.conexaoGlobal;
+            DocumentoHelper dh = new DocumentoHelper(ligacao);
+            dh.save(documento);
+            return RedirectToAction("Listar", "Documento");
+        }
+
+    }
+}
---
+++ 03 06 2024/CoreSQL/CoreSQL/Program.cs
@@ -0,0 +1,18 @@
+internal class Program {
+
+    public static string conexaoGlobal = "server=EDP-JR-10H2\\SQL14; database=GestaoDocumental; uid=IUser; pwd=IUser; TrustServerCertificate=False; Encrypt=False";
+
+
+    private static void Main(string[] args) {
+        var builder = WebApplication.CreateBuilder(args);
+        builder.Services.AddMvc();
+        var app = builder.Build();
+
+        //app.MapGet("/", () => "Hello World!");
+        app.MapControllerRoute(
+           name: "default",
+           pattern: "{controller=Documento}/{action=Listar}/{op?}"
+       );
+        app.Run();
+    }
+}
