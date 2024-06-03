2a37002edd5ec66b9caeea9affed5d0bf7e24efe
brunoxing2004 <mail@brunosilva.co.uk>
2024-06-03 19:05:29 +0100

    appsettings.json

--- 03 06 2024/CoreSQL/CoreSQL/Models/Configuracao.cs
+++ 03 06 2024/CoreSQL/CoreSQL/Models/Configuracao.cs
@@ -2,5 +2,9 @@
 {
     public class Configuracao
     {
+        public string Conexao { get; set; }
+        public string SmtpIP { get; set; }
+        public string UseTLS { get; set; }
+
     }
 }
--- 03 06 2024/CoreSQL/CoreSQL/Program.cs
+++ 03 06 2024/CoreSQL/CoreSQL/Program.cs
@@ -1,7 +1,9 @@
 internal class Program {
 
-    public static string conexaoGlobal = "server=EDP-JR-10H2\\SQL14; database=GestaoDocumental; uid=IUser; pwd=IUser; TrustServerCertificate=False; Encrypt=False";
+    //public static string conexaoGlobal = "server=EDP-JR-10H2\\SQL14; database=GestaoDocumental; uid=IUser; pwd=IUser; TrustServerCertificate=False; Encrypt=False";
 
+    public static string Conector = "";
+    public static string SmtpIP = "";
 
     private static void Main(string[] args) {
         var builder = WebApplication.CreateBuilder(args);
