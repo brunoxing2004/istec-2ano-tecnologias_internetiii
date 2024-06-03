2ab06c752ff184dc4be63ee161687390a11ffd89
brunoxing2004 <mail@brunosilva.co.uk>
2024-06-03 19:00:41 +0100

    Classe Configuracao

--- 03 06 2024/CoreSQL/CoreSQL/obj/Debug/net8.0/CoreSQL.AssemblyInfo.cs
+++ 03 06 2024/CoreSQL/CoreSQL/obj/Debug/net8.0/CoreSQL.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CoreSQL")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9c569ae85a42e8edafb5cb755673c161981482ee")]
 [assembly: System.Reflection.AssemblyProductAttribute("CoreSQL")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CoreSQL")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
